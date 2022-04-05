d786ba40d27842243d1ec487a04135d96ce5d4d6
ellnara <100730699+ellnara@users.noreply.github.com>
2022-04-05 13:29:01 +0400

    Chumiminnnnnnnnnn

---
+++ ConsoleApp3/ConsoleApp3/ExtensionMethod.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    public static class ExtensionMethod
+    {
+
+        public static bool CustomContains(this string word, string find)
+        {
+            int count = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == find[0])
+                {
+                    for (int j = 1; j < find.Length; j++)
+                    {
+                        if (i + j < word.Length)
+                        {
+                            if (word[i + j] == find[j])
+                            {
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+            if (count == find.Length - 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool CustomContains(this string word, char find)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == find)
+                {
+                    return true;
+                }
+
+            }
+            return false;
+
+        }
+        public static bool IsPrime(int number)
+        {
+            int count = 0;
+            for (int i = 0; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count > 2)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
+
+
